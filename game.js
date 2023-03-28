3918842b9a40bae64dfbc019a4d39c9e739ff016
Guilherme Luz <luzguilherme.dev@gmail.com>
2023-03-27 22:01:02 -0300

    Adicionando comentários ao código

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
+// Regra do jogo.
 game = {
+    // Atributos auxiliares.
     firstCard: null,
     secundCard: null,
     lockMode: false,
@@ -17,15 +19,19 @@ game = {
         'uva'
     ],
     cards: null,
+
+    // Crinando  cards baseados na lista de frutas do array fruits.
     createCardsFromFruits: function () {
         this.cards = []
         this.fruits.forEach(fruit => {
-            this.cards.push(this.createPairFromFruits(fruit))
+            this.cards.push(this.createPairFromFruits(fruit));
         })
         this.cards = this.cards.flatMap(pair => pair);
         this.shuflleCards()
-        return this.cards
+        return this.cards;
     },
+
+    // Criando pares com ids diferentes. 
     createPairFromFruits: function (fruit) {
         return [{
             id: this.createId(fruit),
@@ -37,53 +43,66 @@ game = {
             fliped: false
         }]
     },
+
+    // Criando ids aleatórios para identificar cada uma das cartas.
     createId: function (fruit) {
-        return fruit + parseInt(Math.random() * 1000)
+        return fruit + parseInt(Math.random() * 1000);
     },
+
+    // Embaralhando os cards.
     shuflleCards: function () {
-        currentIndex = this.cards.length
+        currentIndex = this.cards.length;
         while (currentIndex != 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--
+            currentIndex--;
             [this.cards[currentIndex], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[currentIndex]];
         }
     },
+
+    // Pegando o card clicado e mudando seu estado para fliped caso ele já não tenha sido clicado.
     setCard: function (id) {
-        let card = this.cards.filter(card => card.id == id)[0]
-        if(this.lockMode || card.fliped){
-            return false
+        let card = this.cards.filter(card => card.id == id)[0];
+        if (this.lockMode || card.fliped) {
+            return false;
         }
         if (!this.firstCard) {
-            this.firstCard = card
-            this.firstCard.fliped = true
-            return true
+            this.firstCard = card;
+            this.firstCard.fliped = true;
+            return true;
         } else {
-            this.secundCard = card
-            this.secundCard.fliped = true
-            this.lockMode = true
-            this.contMoves++
-            return true
+            this.secundCard = card;
+            this.secundCard.fliped = true;
+            this.lockMode = true;
+            this.contMoves++;
+            return true;
         }
     },
+
+    // Verificando se foi formado um par depois de dois cards terem sido clicados.
     checkMate: function () {
         if (!this.firstCard || !this.secundCard) {
-            return false
+            return false;
         }
-        return this.firstCard.icon === this.secundCard.icon
+        return this.firstCard.icon === this.secundCard.icon;
     },
-    gameOver: function(){
-       return this.cards.filter(card => !card.fliped).length == 0
+
+    // Verificado se o jogo foi finalizado.
+    gameOver: function () {
+        return this.cards.filter(card => !card.fliped).length == 0;
     },
-    clearCards: function(){
-        this.firstCard = null
-        this.secundCard = null
-        this.lockMode = false
+
+    // Reiniciando os cards
+    clearCards: function () {
+        this.firstCard = null;
+        this.secundCard = null;
+        this.lockMode = false;
     },
-    unflipedCard: function(){
-        this.firstCard.fliped = false
-        this.secundCard.fliped = false
-        this.clearCards()
+
+    // Mudando o estado dos cards para not fliped 
+    unflipedCard: function () {
+        this.firstCard.fliped = false;
+        this.secundCard.fliped = false;
+        this.clearCards();
     }
- 
-    
+
 }
--- interface.js
+++ interface.js
@@ -1,72 +1,79 @@
+// Declarando constantes .
 const CARD = 'card'
 const FRONT = 'cardFront'
 const BACK = 'cardBack'
 const ICON = 'icon'
 
-startgame()
+// Fução que inicial. 
+startgame();
 
-
+// Criando cards .
 function startgame() {
-    initializeCards(game.createCardsFromFruits())   
+    initializeCards(game.createCardsFromFruits());
 }
 
+// Criando  cards com suas classes e eventos e adicionando no quadro.
 function initializeCards() {
-    let gameBoard = document.getElementById('gameBoard')
-    gameBoard.innerHTML = ''
-    game.contMoves = 0
-    updateViewRanking()    
+    let gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '';
+    game.contMoves = 0;
+    updateViewRanking();
     game.cards.forEach(card => {
         let cardElement = document.createElement('div');
         cardElement.classList.add(CARD);
         cardElement.dataset.icon = card.icon;
         cardElement.id = card.id;
 
-        createCardContent(card, cardElement)
-        cardElement.addEventListener('click', flipCard)
-        gameBoard.appendChild(cardElement)
+        createCardContent(card, cardElement);
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
     })
 
 }
+
+// Função que cria a parte frontal e trazeira dos cards.
 function createCardContent(card, cardElement) {
-    criateFace(FRONT, card, cardElement)
-    criateFace(BACK, card, cardElement)
+    criateFace(FRONT, card, cardElement);
+    criateFace(BACK, card, cardElement);
 }
 
+// Identificando pela classe a face que sera criada e adicionando os elementos de acordo com o lado.
 function criateFace(face, card, cardElement) {
     let cardElementFace = document.createElement('div');
-    cardElementFace.classList.add(face)
+    cardElementFace.classList.add(face);
 
     if (face == FRONT) {
-        let iconElement = document.createElement('img')
+        let iconElement = document.createElement('img');
         iconElement.classList.add(ICON);
         iconElement.src = "./images/" + card.icon + ".png";
-        cardElementFace.appendChild(iconElement)
+        cardElementFace.appendChild(iconElement);
     } else {
         cardElementFace.innerHTML = "&lt/&gt";
     }
-    cardElement.appendChild(cardElementFace)
+    cardElement.appendChild(cardElementFace);
 }
 
+// Adicionando efeito de giro nos cards no momento em que eles forem clicados e verficando se foi formado algum par ou se o jogo ja foi finalizado.
 function flipCard() {
     if (game.setCard(this.id)) {
-        this.classList.add('flip')
-        if (game.secundCard) {         
+        this.classList.add('flip');
+        if (game.secundCard) {
             document.getElementById('contMoves').innerHTML = game.contMoves;
             if (game.checkMate()) {
                 if (game.gameOver()) {
-                    let gameOver = document.getElementById('gameOver')
-                    gameOver.style.display = 'flex'
-                    localStorage.setItem(game.namePlayer, game.contMoves) 
-                    updateViewRanking()
+                    let gameOver = document.getElementById('gameOver');
+                    gameOver.style.display = 'flex';
+                    localStorage.setItem(game.namePlayer, game.contMoves);
+                    updateViewRanking();
                 }
-                game.clearCards()
+                game.clearCards();
             } else {
                 setTimeout(() => {
-                    let firstCardView = document.getElementById(game.firstCard.id)
-                    let secundCardView = document.getElementById(game.secundCard.id)
-                    firstCardView.classList.remove('flip')
-                    secundCardView.classList.remove('flip')
-                    game.unflipedCard(game.namePlayer)
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secundCardView = document.getElementById(game.secundCard.id);
+                    firstCardView.classList.remove('flip');
+                    secundCardView.classList.remove('flip');
+                    game.unflipedCard(game.namePlayer);
                 }, 1000)
             }
 
@@ -74,44 +81,43 @@ function flipCard() {
     }
 }
 
+// Função que reinicia o jogo.
 function reset() {
     inputName = document.getElementById('name')
     if (inputName.value != '' && inputName.value.length <= 10) {
-        let gameOver = document.getElementById('gameOver')
-        gameOver.style.display = 'none'
-        game.clearCards()
-        startgame()
-        game.namePlayer =  inputName.value
-    }else{
-        inputName.style.border = '2px solid red'
-        setTimeout(()=>{
-            inputName.style.border = 'none'
-        },500)
+        let gameOver = document.getElementById('gameOver');
+        gameOver.style.display = 'none';
+        game.clearCards();
+        startgame();
+        game.namePlayer = inputName.value;
+    } else {
+        inputName.style.border = '2px solid red';
+        setTimeout(() => {
+            inputName.style.border = 'none';
+        }, 500)
     }
-   
+
 }
 
-function updateViewRanking(){
-   lista = document.getElementById('list');
-   lista.innerHTML = '';  
-   ranking = sortRanking() 
+// Atualizando o rankeamento com o novo vencedor.
+function updateViewRanking() {
+    lista = document.getElementById('list');
+    lista.innerHTML = '';
+    ranking = sortRanking();
 
-   for(let i =0; i < localStorage.length; i++){
-        if(i <= 10){
-            lista.innerHTML += `<li>${localStorage.key(i)} - ${ranking[i]}</li>`
+    for (let i = 0; i < localStorage.length; i++) {
+        if (i <= 10) {
+            lista.innerHTML += `<li>${localStorage.key(i)} - ${ranking[i]}</li>`;
         }
-   }
- 
+    }
 }
-
-function sortRanking(){
-    let ranking = []
-    for(let i = 0 ; i < localStorage.length; i++){
-        ranking.push(localStorage.getItem(localStorage.key(i)))
+// Ordenando a lista de vencedores em ordem crescente.
+function sortRanking() {
+    let ranking = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        ranking.push(localStorage.getItem(localStorage.key(i)));
     }
     ranking.sort()
-    console.log(ranking)
-    
-    return ranking
-    
+    console.log(ranking);
+    return ranking;
 }
